215e91ccbb7b80286ece0061b5910b61fb7b4d31
AppleTesla <jkschrei@gmail.com>
2021-04-03 04:10:37 -1000

    Initial Commit

---
+++ Kekoa/KekoaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KekoaApp.swift
+//  Kekoa
+//
+//  Created by Jaron Schreiber on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct KekoaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
